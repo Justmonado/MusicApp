dd7feb3b45ce10823700c82b70ccec5766c763f8
Alfonso Soots <alfonso_soots@hotmail.com>
2026-05-13 22:18:15 -0600

    Otra Vez

---
+++ app/src/main/java/com/pajasoft/musicapp/models/Music.kt
@@ -0,0 +1,12 @@
+package com.pajasoft.musicapp.models
+
+data class Album (
+    val id : String,
+    val title: String,
+    val artist: String,
+    val description: String,
+    val image: String
+)
+
+
+
